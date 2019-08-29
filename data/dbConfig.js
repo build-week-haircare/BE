075d8562e2be67e965f37a6d4ed105a96f4a8b2a
alasalle <angela.lasalle91@gmail.com>
2019-08-28 23:08:44 -0400

    ugh

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -4,3 +4,5 @@ const knexConfig = require('../knexfile.js');
 const dbEnv = process.env.DB_ENV || 'development';
 
 module.exports = knex(knexConfig[dbEnv]);
+
+// "heroku-postbuild": "knex migrate:latest && knex seed:run"
